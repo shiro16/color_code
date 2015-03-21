63810651032421dde42599e558223f4ab6d46a47
shiro16 <nyanyanyawan24@gmail.com>
2015-03-22 03:41:21 +0900

    Add specs

---
+++ spec/color_code/hsl_spec.rb
@@ -0,0 +1,129 @@
+require 'spec_helper'
+
+describe ColorCode::HSL do
+  let(:hue) { rand(360) }
+  let(:saturation) { rand(100) }
+  let(:luminance) { rand(100) }
+
+  describe '#initialize' do
+    context 'parameter code' do
+      subject { ColorCode::HSL.new('#ff0000') }
+
+      it { expect(subject.h).to eq(0) }
+      it { expect(subject.s).to eq(100) }
+      it { expect(subject.l).to eq(50) }
+    end
+
+    context 'parameter hsl' do
+      subject { ColorCode::HSL.new(h: hue, s: saturation, l: luminance) }
+
+      it { expect(subject.h).to eq(hue) }
+      it { expect(subject.s).to eq(saturation) }
+      it { expect(subject.l).to eq(luminance) }
+    end
+
+    context 'hsl over some velue' do
+      subject { ColorCode::HSL.new(h: 361, s: 101, l: 101) }
+
+      it { expect{ subject }.to raise_error(ArgumentError) }
+    end
+  end
+
+  describe '#to_s' do
+    subject { ColorCode::HSL.new(h: 0, s: 100, l: 50).to_s }
+
+    it { expect(subject).to eq('#ff0000') }
+  end
+
+  describe '#to_a' do
+    subject { ColorCode::HSL.new(h: hue, s: saturation, l: luminance).to_a }
+
+    it { expect(subject).to eq([hue, saturation, luminance])}
+  end
+
+  describe '#to_hash' do
+    subject { ColorCode::HSL.new(h: hue, s: saturation, l: luminance).to_hash }
+
+    it { expect(subject).to eq({h: hue, s: saturation, l: luminance}) }
+  end
+
+  describe '#to_rgb' do
+    subject { ColorCode::HSL.new(h: 0, s: 100, l: 50).to_rgb }
+
+    it { expect(subject.r).to eq(255) }
+    it { expect(subject.g).to eq(0) }
+    it { expect(subject.b).to eq(0) }
+  end
+
+  context 'private methods' do
+    describe '#convert_rgb' do
+      context 'luminance under 50' do
+        let(:luminance) { 25 }
+
+        it 'hue 0-59' do
+          hsl = ColorCode::HSL.new(h: 30, s: 50, l: luminance)
+          expect(hsl.send(:convert_rgb)).to eq([96, 32, 32])
+        end
+
+        it 'hue 60-119' do
+          hsl = ColorCode::HSL.new(h: 90, s: 50, l: luminance)
+          expect(hsl.send(:convert_rgb)).to eq([32, 96, 32])
+        end
+
+        it 'hue 120-179' do
+          hsl = ColorCode::HSL.new(h: 150, s: 50, l: luminance)
+          expect(hsl.send(:convert_rgb)).to eq([32, 96, 32])
+        end
+
+        it 'hue 180-239' do
+          hsl = ColorCode::HSL.new(h: 210, s: 50, l: luminance)
+          expect(hsl.send(:convert_rgb)).to eq([32, 32, 96])
+        end
+
+        it 'hue 240-299' do
+          hsl = ColorCode::HSL.new(h: 270, s: 50, l: luminance)
+          expect(hsl.send(:convert_rgb)).to eq([32, 32, 96])
+        end
+
+        it 'hue 300-359' do
+          hsl = ColorCode::HSL.new(h: 330, s: 50, l: luminance)
+          expect(hsl.send(:convert_rgb)).to eq([96, 32, 32])
+        end
+      end
+
+      context 'luminance over 50' do
+        let(:luminance) { 75 }
+
+        it 'hue 0-59' do
+          hsl = ColorCode::HSL.new(h: 30, s: 50, l: luminance)
+          expect(hsl.send(:convert_rgb)).to eq([223, 159, 159])
+        end
+
+        it 'hue 60-119' do
+          hsl = ColorCode::HSL.new(h: 90, s: 50, l: luminance)
+          expect(hsl.send(:convert_rgb)).to eq([159, 223, 159])
+        end
+
+        it 'hue 120-179' do
+          hsl = ColorCode::HSL.new(h: 150, s: 50, l: luminance)
+          expect(hsl.send(:convert_rgb)).to eq([159, 223, 159])
+        end
+
+        it 'hue 180-239' do
+          hsl = ColorCode::HSL.new(h: 210, s: 50, l: luminance)
+          expect(hsl.send(:convert_rgb)).to eq([159, 159, 223])
+        end
+
+        it 'hue 240-299' do
+          hsl = ColorCode::HSL.new(h: 270, s: 50, l: luminance)
+          expect(hsl.send(:convert_rgb)).to eq([159, 159, 223])
+        end
+
+        it 'hue 300-359' do
+          hsl = ColorCode::HSL.new(h: 330, s: 50, l: luminance)
+          expect(hsl.send(:convert_rgb)).to eq([223, 159, 159])
+        end
+      end
+    end
+  end
+end
---
+++ spec/color_code/rgb_spec.rb
@@ -0,0 +1,138 @@
+require 'spec_helper'
+
+describe ColorCode::RGB do
+  let(:red) { rand(255) }
+  let(:green) { rand(255) }
+  let(:blue) { rand(255) }
+
+  describe '#initialize' do
+    context 'parameter code' do
+      subject { ColorCode::RGB.new('#ff1001') }
+
+      it { expect(subject.r).to eq(255) }
+      it { expect(subject.g).to eq(16) }
+      it { expect(subject.b).to eq(1) }
+    end
+
+    context 'parameter rbg' do
+      subject { ColorCode::RGB.new(r: red, g: green, b: blue) }
+
+      it { expect(subject.r).to eq(red) }
+      it { expect(subject.g).to eq(green) }
+      it { expect(subject.b).to eq(blue) }
+    end
+
+    context 'rgb over 255' do
+      subject { ColorCode::RGB.new(r: 256, g: 256, b: 256) }
+
+      it { expect{ subject }.to raise_error(ArgumentError) }
+    end
+  end
+
+  describe '#to_s' do
+    subject { ColorCode::RGB.new(r: 255).to_s }
+
+    it { expect(subject).to eq('#ff0000') }
+  end
+
+  describe '#to_a' do
+    subject { ColorCode::RGB.new(r: red, g: green, b: blue).to_a }
+
+    it { expect(subject).to eq([red, green, blue]) }
+  end
+
+  describe '#to_hash' do
+    subject { ColorCode::RGB.new(r: red, g: green, b: blue).to_hash }
+
+    it { expect(subject).to eq({ r: red, g: green, b: blue }) }
+  end
+
+  describe '#to_hsl' do
+    subject { ColorCode::RGB.new(r: 255).to_hsl }
+
+    it { expect(subject.to_rgb.to_s).to eq('#ff0000') }
+    it { expect(subject.h).to eq(0) }
+    it { expect(subject.s).to eq(100) }
+    it { expect(subject.l).to eq(50) }
+  end
+
+  context 'private methods' do
+    describe '#parse_code' do
+      subject { ColorCode::RGB.new.send(:parse_code, code) }
+
+      context 'code is match' do
+        let(:code) { '#fF0000' }
+
+        it { expect(subject).to eq([255, 0, 0]) }
+      end
+
+      context 'code does not match' do
+        let(:code) { '#GG0000' }
+
+        it { expect{ subject }.to raise_error(ArgumentError) }
+      end
+    end
+
+    describe '#h' do
+      subject { rgb.send(:h) }
+
+      context 'rgb all the same value' do
+        let(:rgb) { ColorCode::RGB.new }
+
+        it { expect(subject).to eq(0) }
+      end
+
+      context 'max value r' do
+        let(:rgb) { ColorCode::RGB.new(r: 150, g: 100, b: 100) }
+
+        it { expect(subject).to eq(0) }
+      end
+
+      context 'max value g' do
+        let(:rgb) { ColorCode::RGB.new(r: 100, g: 150, b: 100) }
+
+        it { expect(subject).to eq(120) }
+      end
+
+      context 'max value b' do
+        let(:rgb) { ColorCode::RGB.new(r: 100, g: 100, b: 150) }
+
+        it { expect(subject).to eq(240) }
+      end
+    end
+
+    describe '#s' do
+      subject { rgb.send(:s) }
+
+      context 'converge under 128' do
+        let(:rgb) { ColorCode::RGB.new(r: 150, g: 100, b: 100) }
+
+        it { expect(subject).to eq(20) } 
+      end
+
+      context 'converge over 128' do
+        let(:rgb) { ColorCode::RGB.new(r: 255, g: 255, b: 255) }
+
+        it { expect(subject).to eq(0) } 
+      end
+    end
+
+    describe '#l' do
+      subject { ColorCode::RGB.new(r:255).send(:l) }
+
+      it { expect(subject).to eq(50) }
+    end
+
+    describe '#max' do
+      subject { ColorCode::RGB.new(r: 1, g: 2, b: 3).send(:max) }
+
+      it { expect(subject).to eq(3) }
+    end
+
+    describe '#min' do
+      subject { ColorCode::RGB.new(r: 1, g: 2, b: 3).send(:min) }
+
+      it { expect(subject).to eq(1) }
+    end
+  end
+end
